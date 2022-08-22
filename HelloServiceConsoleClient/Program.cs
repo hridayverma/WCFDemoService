37975e3c43670f4b359f12fe12ebd2e15d586ee9
hridayverma <hrid.06@gmail.com>
2022-08-22 16:52:42 +0530

    Add project files.

---
+++ HelloServiceConsoleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelloServiceConsoleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyHelloService.HelloServiceClient client = new
+                                    MyHelloService.
+                                    HelloServiceClient("NetTcpBinding_IHelloService");
+            client.DoWork();
+            Console.WriteLine("Sum:"+client.Addition(20,30));
+
+            client.Close();
+        }
+    }
+}
---
+++ HelloServiceWebClient/WebForm1.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloServiceWebClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MyService.HelloServiceClient client = new MyService.
+                                         HelloServiceClient
+                                         ("BasicHttpBinding_IHelloService");
+            client.DoWork();
+            Response.Write(client.Message("Calling WCF service from web client"));
+        }
+    }
+}
